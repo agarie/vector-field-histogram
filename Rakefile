2ead1358b0302ae177fcbfe749184a82fd7d20bb
Carlos Agarie <carlos.agarie@gmail.com>
2012-10-29 21:21:10 -0700

    Added an example showing how to use it.
    ... unfortunately, it isn't working yet. I *think* I know where the segmentation fault is coming from, so I might be able to fix it today.

--- Rakefile
+++ Rakefile
@@ -1,5 +1,15 @@
-task :default => ['vfh.o']
+task :default => [:compile, :example]
+
+task :compile => ['vfh.o']
+
+task :example => ['no-velocity'] do
+  sh "./no-velocity"
+end
 
 file 'vfh.o' => ['vfh.c', 'vfh.h'] do
   sh "gcc vfh.c -c -o vfh.o -Wall"
+end
+
+file 'no-velocity' => ['examples/no-velocity.c', 'vfh.o', 'vfh.h'] do
+  sh "gcc examples/no-velocity.c vfh.o -I. -o no-velocity"
 end
---
+++ examples/no-velocity.c
@@ -0,0 +1,85 @@
+/*
+** no-velocity.c
+**
+** Author: Carlos Agarie Junior
+**
+** This is an example file used to show how to setup basic usage of the Virtual
+** Field Histogram.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../vfh.h"
+
+#define MEASUREMENTS 1000
+#define BEST_DIRECTION 75 /* [degrees] */
+
+int main(void) {
+	int i, position_x, position_y;
+	
+	/* Declaration of the needed data structures. */
+	grid_t * certainty_grid;
+	range_measure_t measure[MEASUREMENTS];
+	hist_t * polar_histogram;
+	control_signal_t control_signal;
+	
+	/*
+	** Initialization of the grid and the histogram.
+	*/
+
+	certainty_grid = grid_init(11, 0.2);
+	polar_histogram = hist_init(2, 20, 10, 5);
+	
+	/* Are the initializations ok? */
+	if (certainty_grid == NULL) return -1;
+	if (certainty_grid->cells == NULL) return -1;
+	if (polar_histogram == NULL) return -1;
+	if (polar_histogram->densities == NULL) return -1;
+	
+	/*
+	** Fake measures.
+	*/
+
+	printf("Measures\n");
+		
+	for (i = 0; i < MEASUREMENTS; ++i) {
+		measure[i].direction = (int) ((360.0 * rand()) / RAND_MAX) ; /* [degrees] */
+		measure[i].distance = (int) (((130.0 * rand()) / RAND_MAX) + 20); /* [cm] */
+		
+		if (i < 50)
+			printf("\t%2d: %3d [cm], %3d [degrees]\n", i, measure[i].distance,
+																										measure[i].direction);
+	}
+	
+	if (i > 50) printf("\t...\n");
+
+	/* Let's assume the 'robot' is in the middle of the grid. */
+	position_x = position_y = (certainty_grid->dimension + 1) / 2;
+
+	/* Add the information from the measures to the grid. */
+	printf("\nUpdating the certainty grid...\n");
+	grid_update(certainty_grid, position_x, position_y, measure, MEASUREMENTS);
+		
+	/*
+	** Calculating the control signals.
+	*/
+	
+	/* Generate the histogram for the current grid. */
+	printf("\nUpdating the polar histogram...\n");
+	hist_update(polar_histogram, certainty_grid);
+	
+	/* What's the next direction? */
+	control_signal.direction = calculate_direction(polar_histogram,
+																								BEST_DIRECTION);
+	
+	printf("\nNext direction: %d [degrees]\n", control_signal.direction);
+
+	free(certainty_grid);
+	free(polar_histogram);
+	
+	certainty_grid = NULL;
+	polar_histogram = NULL;
+
+	return 0;
+}
