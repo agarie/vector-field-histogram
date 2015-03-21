0fb803ce89749152373eb8f16d264e9bec9e040f
Carlos Agarie <carlos.agarie@gmail.com>
2015-03-21 16:08:25 -0300

    Adding minor documentation to histogram_grid.h

--- histogram_grid.h
+++ histogram_grid.h
@@ -1,27 +1,35 @@
+/**
+ * @file histogram_grid.h
+ * @brief Define what a Histogram Grid is and how to manipulate it.
+ */
+
 #ifndef HISTOGRAM_GRID_H
 #define HISTOGRAM_GRID_H
 
-/* Certainty grid. */
+/* Histogram grid.
+ *
+ * Use `i * dimension + j` to iterate over the `cells`.
+ * */
 typedef struct {
-  int dimension;
-  int resolution;
-  int *cells;
+  int dimension; /* Dimension in number of cells. */
+  int resolution; /* Centimeters per cell. */
+  int *cells; /* The obstacle density in each cell. */
 } grid_t;
 
-/* A rangefinder measurement. */
+/* A rangefinder measurement.
+ *
+ */
 typedef struct {
   int direction; /* [degrees] */
   unsigned long distance; /* [cm] */
 } range_measure_t;
 
-/* Certainty grid. */
-
-/* grid_init: Return a pointer to an empty grid. NULL otherwise. */
+/* grid_init: Return a pointer to an empty (all zeros) grid. NULL otherwise. */
 grid_t * grid_init(int dimension, int resolution);
-
-/* grid_update: Update grid's cells with an array of sensor readings. */
-int grid_update(grid_t * grid, int pos_x, int pos_y, range_measure_t data);
 
 /* get_moving_window: Get a sub-grid of the grid centered in (x, y). */
 grid_t * get_moving_window(grid_t * grid, int pos_x, int pos_y, int dimension);
+
+/* grid_update: Update grid's cells with an array of sensor readings. */
+int grid_update(grid_t * grid, int pos_x, int pos_y, range_measure_t data);
 #endif
